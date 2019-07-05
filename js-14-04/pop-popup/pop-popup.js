9d18d0b6c722074cb429adfaac4c713daf710197
Theo Armour <theo-armour@users.noreply.github.com>
2019-07-05 13:15:22 -0700

    popfooter temp fix

--- js-14-04/pop-popup/pop-popup.js
+++ js-14-04/pop-popup/pop-popup.js
@@ -34,13 +34,14 @@ POP.getMenuDivPopup = function() {
 	divDragMoveHeader.addEventListener( 'touchstart', DMV.onTouchStart, false );
 	divDragMoveHeader.addEventListener( 'touchmove', DMV.onTouchMove, false );
 
+	//<button onclick=POP.requestFile("../../pages/license.md",divDragMoveContent); style=width:2rem;background:#ded;>⚖️</button>&nbsp;
+
 	POP.footer =
 
 		`<div >
 			<button onclick=POP.requestFile("../popup.md",divDragMoveContent); style=width:2rem;background:#ded;>🏠</button>&nbsp;
-			<button onclick=POP.requestFile("../../pages/license.md",divDragMoveContent); style=width:2rem;background:#ded;>⚖️</button>&nbsp;
 
-			<span id=POPspanFooter >v${ POP.version } - ${ POP.date } </span
+			<span id=POPspanFooter >v${ POP.version } - ${ POP.date } </span>
 		</div>`;
 
 	const txt = "lorem ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem. ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur?";
