5e6ebf032dbc94cc261e9f48246fe0f013223b0b
Theo Armour <theo-armour@users.noreply.github.com>
2019-07-05 00:14:06 -0700

    oops

--- js-14-04/pop-popup/pop-popup.js
+++ js-14-04/pop-popup/pop-popup.js
@@ -39,11 +39,11 @@ POP.getMenuDivPopup = function() {
 
 		`<div >
 			<button onclick=POP.requestFile("../popup.md",divDragMoveContent); style=width:2rem;background:#ded;>🏠</button>&nbsp;
+<!--
 			<button onclick=POP.requestFile("${ MNU.footerPopupUrl }pages/license.md",divDragMoveContent);; style=width:2rem;background:#dde;>⚖️</button>
+-->
 			<span id=POPspanFooter >v${ POP.version } - ${ POP.date } </span
 		</div>`;
-
-
 
 	const txt = "lorem ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem. ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur?";
 
