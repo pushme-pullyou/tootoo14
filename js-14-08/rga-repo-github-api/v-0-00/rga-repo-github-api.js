f6b27c4f358002e03a392ae14e6459c60208d2a4
Theo Armour <theo-armour@users.noreply.github.com>
2019-09-17 21:47:23 -0700

    gbxml first commit

--- js-14-08/rga-repo-github-api/v-0-00/rga-repo-github-api.js
+++ js-14-08/rga-repo-github-api/v-0-00/rga-repo-github-api.js
@@ -8,15 +8,15 @@ const RGA = {
 	"script": {
 
 		"copyright": "Copyright 2019 pushMe-pullYou authors.",
-		"date": "2019-08-27",
-		"description": "template",
+		"date": "2019-09-17",
+		"description": "Get repos",
 		"license": "MIT License",
 		"title": "Repos viaGitHub API RGA",
 		"urlHelpFile":
 			"https://pushme-pullyou.github.io/tootoo14/js-14-08/tmp-template/tmp-template.md",
 		"urlSourceCode":
 			"https://github.com/pushme-pullyou/tootoo14/tree/master/js-14-08/tmp-template",
-		"version": "0.14.08-0tmp"
+		"version": "0.14.08-1rga"
 
 	}
 
@@ -33,14 +33,13 @@ RGA.getMenuReposUser = function() {
 
 	const htm =
 		`
-			<details ontoggle="RGAdivRepos.innerHTML=RGA.setMenuUserRepos();" >
+			<details ontoggle="RGA.setMenuUserRepos();" >
 
 				<summary id=RGAsumS >Repos</summary>
 
-				<button id=RGAbut class=butHelp onclick="POP.setPopupShowHide(RGAbut,RGA.script.urlHelpFile);" >?</button>
+				<a href="${ RGA.urlHelpFile  }" style=float:right; >?</a>
 
 				<div id=RGAdivRepos ></div>
-
 
 			</details>
 
@@ -51,12 +50,7 @@ RGA.getMenuReposUser = function() {
 };
 
 
-
-
-
-RGA.setMenuUserRepos = function( path = '' ) {
-
-	//console.log( 'path', path );
+RGA.setMenuUserRepos = function( path = "" ) {
 
 	const str = RGA.accessToken ? "?access_token=" + RGA.accessToken : "";
 
@@ -66,9 +60,7 @@ RGA.setMenuUserRepos = function( path = '' ) {
 
 	RGA.requestFile( url ); // to do: make request only once and triage thereafter
 
-
 };
-
 
 
 RGA.requestFile = function( url ) {
@@ -85,62 +77,21 @@ RGA.requestFile = function( url ) {
 
 RGA.callbackGitHubUserRepos = function( xhr ) {
 
-
-
 	const response = xhr.target.response;
 	const items = JSON.parse( response );
-	console.log( 'items ', items );
 
-	RGA.menuItems = items;
-
-	//if ( items.message !== "Not Found" ) { alert( items.message ); return; }
 	if ( items.message ) { console.log( 'error', items.message ); return; } //breadcrumbs??
 
-	const repos = RGA.getRepos( items );
-
-	// const htmFiles = RGA.getFilesFromContents( items );
-
-	// const htmHelp =
-	// `
-	// 	<p>Click any <a href=${ RGA.urlSourceCode } >${ RGA.iconInfo }</a> icon to view source code on GitHub.
-
-	// 	<p>Click any &#x2750; icon to go full screen & obtain a link to the individual file.</p>
-
-	// 	<p>Tooltips provide file size.</p>
-	// `;
-
-
-	RGAdivRepos.innerHTML = repos; // + htmFiles + htmHelp;
-
-
-}
-
-
-
-RGA.getRepos = function( items ) {
-
-	let htm = "";
-
-	const len = FGA.pathRepo.length;
-
-	for ( let item of items ) {
-
-
-
-			// why does this not work? item.path.split( "/" ).pop()
-		htm +=
+	const htm = items.map( item =>
 		`
 			<div style=margin-bottom:8px;padding:0; >
 				<a href=JavaScript:FGA.setMenuGitHubPathFileNames("${ item.url }"); >
-					&#x1f4c1; ${ item.name }
+					📁 ${ item.name }
 				</a>
 			</div>
-		`;
+		`
+	).join( "" );
 
-	}
-
-
-	return htm;
+	RGAdivRepos.innerHTML = htm;
 
 };
-
