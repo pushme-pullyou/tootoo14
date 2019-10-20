5eaeeefa5c87074ab3d7b67fa6ed125fdf783fcf
Theo Armour <theo-armour@users.noreply.github.com>
2019-10-19 17:09:40 -0700

    gpat updates

--- js-14-08/gat-github-access-token/gat-github-access-token.js
+++ js-14-08/gat-github-access-token/gat-github-access-token.js
@@ -5,30 +5,27 @@
 const GAT = {
 
 	"copyright": "Copyright 2019 PushMe-PullYou authors",
-	"date": "2019-06-08",
-	"description": "GitHub access token",
-	"helpFile": "../gat-github-access-token/README.md",
+	"date": "2019-10-19",
+	"description": "Personal access tokens function like ordinary OAuth access tokens. They can be used instead of a password for Git over HTTPS, or can be used to authenticate to the API over Basic Authentication.",
+	"sourceCode": "https://github.com/pushme-pullyou/tootoo14/tree/master/js-14-08/gat-github-access-token",
 	"license": "MIT License",
-	"version": "0.14.06-0gat",
+	"version": "0.14.08-1gat",
 };
 
 
 
-GAT.getMenuGitHubAccessToken = function() {
+GAT.getMenuGitHubAccessToken = function () {
 
-	//GAT.divContents = divContents;
-	//GAT.urlGitHubSource = "https://github.com/" + GAT.user + "/" + GAT.repo;
-	//GAT.urlGitHubApiContents = 'https://api.github.com/repos/' + GAT.user + "/" + GAT.repo + '/contents/' + GAT.pathRepo;
 	GAT.accessToken = localStorage.getItem( 'githubAccessToken' ) || '';
 
 	const htm =
-	`
+		`
 		<details>
 
-			<summary>GitHub API Access Token </summary>
+			<summary>GitHub API Personal Access Token </summary>
 
 			<div>
-				<button id=butGAT class=butHelp onclick="GAT.setViewHelp(butGAT,GAT.helpFile);" >?</button>
+				<button id=butGAT class=butHelp onclick="GATdiaHelp.open=!GATdiaHelp.open;" >?</button>
 			</div>
 
 			<p>
@@ -42,8 +39,25 @@ GAT.getMenuGitHubAccessToken = function() {
 				</button>
 			</p>
 
-
 		</details>
+
+		<dialog id=GATdiaHelp onclick=GATdiaHelp.close() >
+
+			<h1>GitHub API Personal Access Token</h1>
+
+			<div>
+
+				<p>${ GAT.description }</p>
+
+				<p><a href="${ GAT.sourceCode }" target="_blank">Source code</a></p>
+
+				<p>Version: ${ GAT.version } - Update: ${ GAT.date } </p>
+
+				<p>${ GAT.copyright }. ${ GAT.license }.</p>
+
+			<div>
+
+		</dialog>
 	`;
 
 	return htm;
@@ -51,22 +65,7 @@ GAT.getMenuGitHubAccessToken = function() {
 };
 
 
-
-GAT.setViewHelp = function () {
-
-	if ( window.POP ) {
-
-		POP.setPopupShowHide(butGAT,GAT.helpFile);
-
-	} else {
-
-
-	}
-
-};
-
-
-GAT.setGitHubAccessToken = function( accessToken ) {
+GAT.setGitHubAccessToken = function ( accessToken ) {
 
 	console.log( 'accessToken', accessToken );
 
@@ -78,9 +77,7 @@ GAT.setGitHubAccessToken = function( accessToken ) {
 
 
 
-
-
-GAT.setViewRateLimits = function( button, target = divContents ) {
+GAT.setViewRateLimits = function ( button, target = divContents ) {
 	//console.log( 'button', button );
 
 	const url = "https://api.github.com/rate_limit";
@@ -90,19 +87,26 @@ GAT.setViewRateLimits = function( button, target = divContents ) {
 	xhr.onload = callback;
 	xhr.send( null );
 
-	function callback( xhr ) {
+	function callback ( xhr ) {
 
 		const htm =
-		`
-			<h3>GitHub rate limits status</h3>
+			`
+			<dialog open onclick=this.close(); style="max-height: 50%; overflow: auto;">
 
-			<p>
-				Some of the scripts in this app use the GitHub Developer API.
-				The API which has rate limits that reset every hour.
-				See <a href="https://developer.github.com/v3/#rate-limiting" target="_blank">developer.github.com/v3</a>.
-			</p>
+				<h1>GitHub rate limits status</h1>
 
-			<pre> ${ xhr.target.response } </pre>
+				<div>
+					<p>
+						Some of the scripts in this app use the GitHub Developer API.
+						The API which has rate limits that reset every hour.
+						See <a href="https://developer.github.com/v3/#rate-limiting" target="_blank">developer.github.com/v3</a>.
+					</p>
+
+					<pre> ${ xhr.target.response } </pre>
+
+				</div>
+
+			</dialog>
 		`;
 
 		target.innerHTML += htm;
